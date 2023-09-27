4c5f06802e0ff2ab74fada87548de075e10fb7ab
유우카(优香) <142193893+yuuka66@users.noreply.github.com>
2023-09-27 00:00:10 +0000

    update

---
+++ AjaxData/ProLicenceUsersTableJS.js
@@ -0,0 +1,375 @@
+ProLicenceUsersTable = {};
+ProLicenceUsersTable = {
+    "ProLicenceUsers": {
+        "1ad344c0-40c4-4ffe-b1b0-142f6918d437": {
+            "reason": "LMauthor",
+            "notes": "Jimboy3100"
+        },
+        "2f9c0723-d7a4-4e2b-8973-bef9fc7826ed": {
+            "reason": "Moderator",
+            "notes": "Darkspace"
+        },
+        "83cc1f6e-6ccf-4e05-8bf2-46cf1e26b6a1": {
+            "reason": "Moderator",
+            "notes": "New-Jack"
+        },   
+        "32254495-c62d-4d0c-b66e-3d81010d401d": {
+            "reason": "℄🌀idris",
+            "notes": "℄🌀"
+        }, 	    
+        "7bea1cb9-7e14-4e14-824c-c2682036b0a6": {
+            "reason": "℄🌀βurNouT",
+            "notes": "℄🌀"
+        },
+        "204c1426-c4c9-46af-b29d-5fa9d4d8704a": {
+            "reason": "℄🌀ilyes",
+            "notes": "℄🌀"
+        }, 		
+        "a8829a04-9191-47aa-b29e-c3ee1e23a102": {
+            "reason": "℄🌀alicicine",
+            "notes": "℄🌀"
+        }, 			
+        "c8c0f123-2758-410b-a952-e4c4898319e5": {
+            "reason": "℄🌀ahmad",
+            "notes": "℄🌀"
+        }, 				       
+        "e18af28b-d1de-4645-967f-9b95736e8817": {
+            "reason": "Hendrik Schönfelder",
+            "notes": "Youtuber"
+        },
+        "a3742f28-059a-4ea1-abf7-566e69ec97f4": {
+            "reason": "Moderator",
+            "notes": "Snez"
+        }, 
+        "5b26499a-ccd0-4cdb-9c0e-9375b9d7cd4c": {
+            "reason": "Shiro",
+            "notes": "Advertiser-Japan-Fb"
+        }, 
+        "46eab069-2e6a-449b-ba20-494dbd6e312a": {
+            "reason": "Hano",
+            "notes": "Admin"
+        }, 	
+        "ad800ce0-8612-4fab-af1d-715f024c24e5": {
+            "reason": "Hano",
+            "notes": "Admin"
+        }, 	
+        "1704daba-4f74-436f-9b17-a2f69a9e910b": {
+            "reason": "Hano",
+            "notes": "Admin"
+        }, 	
+        "396023b0-fc28-4638-b4e4-f836620ba746": {
+            "reason": "Hilde",
+            "notes": "Admin"
+        }, 
+        "b74efc8b-4e87-4c39-a7ac-a289e78eb012": {
+            "reason": "Hilde",
+            "notes": "Admin"
+        }, 	    
+        "0c8e1929-d2ce-4738-84d5-3c7f7d10f4c6": {
+            "reason": "Junky",
+            "notes": "Admin"
+        }, 	
+        "81831afa-0a11-4fce-82b3-81067f6cc9bc": {
+            "reason": "Shiro",
+            "notes": "Advertiser-Japan-Google"
+        }, 
+        "39ebbaa0-8b5f-485e-a103-754a0379f287": {
+            "reason": "FriendOfBotplayer",
+            "notes": "Overkill"
+        }, 	    
+        "7bb006df-2994-4e4c-9ed8-1f40aa541196": {
+            "reason": "coronaagar",
+            "notes": "friend"
+        }, 	
+        "4220bddd-7bd6-4f93-89cf-579903eff2bc": {
+            "reason": "Donate",
+            "notes": "Cla"
+        }, 
+        "a9b2d2f1-ed80-4a9c-bf9f-73654e8cdd34": {
+            "reason": "Donate",
+            "notes": "Jeison"
+        }, 
+        "9d89a4ac-2a2b-4b97-bd52-a40c14ec3cee": {
+            "reason": "Donate",
+            "notes": "BehnanKaviari"
+        },		
+        "fdcd9b88-f083-41bb-b444-9bd559278de9": {
+            "reason": "Donate",
+            "notes": "TeachMe"
+        },
+        "40252edd-4ec4-41a0-865f-aa9d967a3706": {
+            "reason": "Nov2021",
+            "notes": "Java"
+        }, 	    
+        "21e8e56d-b2b7-44a0-a35e-e9004309fa2a": {
+            "reason": "Donate",
+            "notes": "Kristina"
+        },	    
+        "ca2f18d3-2023-48b2-9b71-1c8f5b1a10f3": {
+            "reason": "Donate",
+            "notes": "Imagine74"
+        },		
+        "b8ae7f49-30b2-4354-93e9-84d44dda84b4": {
+            "reason": "Donate",
+            "notes": "JonathanMedrano"
+        },	
+        "43f70f95-dd23-441b-b8c7-95787db46062": {
+            "reason": "HendriksFriend",
+            "notes": "Riven"
+        },	
+        "c930db58-ff74-429b-a05b-099388031f50": {
+            "reason": "Donate",
+            "notes": "SkyOmer"
+        }, 	
+        "5fe03f88-f383-4f35-8bde-2b420f436d72": {
+            "reason": "Donate",
+            "notes": "fkOff"
+        }, 	
+        "b4c56d29-869b-4d08-a572-44f547440e95": {
+            "reason": "Donate",
+            "notes": "TheGem"
+        }, 	
+        "0998828c-a1a4-46b3-bc54-b8647042d5a4": {
+            "reason": "Donate",
+            "notes": "TheGem2"
+        }, 	
+        "77cd1c7f-a602-457f-b192-8c8b55b1fc46": {
+            "reason": "Donate",
+            "notes": "Alphamenzo"
+        },	
+        "43b81454-bb19-4c52-941f-e2231b8bfea3": {
+            "reason": "friendOfJan",
+            "notes": "Jamie_23744"
+        }, 	    
+        "17914d3b-6b8f-468a-bb6b-4d91d3c3f670": {
+            "reason": "GoodWillForSon",
+            "notes": "jebak"
+        }, 	
+        "5f4d77aa-65ba-497c-b496-91fe22adeb1a": {
+            "reason": "TheLastUpdate",
+            "notes": "Botpia"
+        },  
+        "99263314-0c92-4bf4-8335-895f2e7fae4d": {
+            "reason": "Donate",
+	     "notes": "Geo"
+        }, 	    
+        "bbf24317-be3f-4dac-a4d8-f1770434ace5": {
+            "reason": "Donate",
+            "notes": "Meelis"
+        }, 	
+        "ed026e2f-7cea-4fd0-beff-84ae2711e67f": {
+            "reason": "randomfriendJimboy3100",
+            "notes": "suryox"
+        }, 	    
+        "84222701-39e7-454e-9e41-bc8fd4f468d2": {
+            "reason": "Donate",
+            "notes": "Siah"
+        },     
+        "87b8a4f8-0cb4-448d-bedc-358a3010c7e6": {
+            "reason": "Donate",
+            "notes": "Fiku"
+        },	
+        "2bb8b084-d1e0-4ce9-936b-e10d82f0b937": {
+            "reason": "Donate",
+            "notes": "ChristianDíaz"
+        },	
+        "60159e63-79ed-4995-bf5d-fc85fe784fa8": {
+            "reason": "ellhnas",
+            "notes": "Paixe"
+        },		
+        "83df7615-1c6a-4a9d-9d86-1eba11d9589c": {
+            "reason": "BeggerJapan",
+            "notes": "padelsendpie"
+        }, 			
+        "7aa2d964-ac82-497c-aa34-25cc342299bd": {
+            "reason": "Donate",
+            "notes": "CentralAsia"
+        }, 		
+        "3361c3f4-3d94-49d2-b19c-c1b8241d4574": {
+            "reason": "Give@20200910",
+            "notes": "AngelDeltaServer"
+        },		
+        "e8a49e32-7f71-4449-b1ae-1bf2fe4dc3b": {
+            "reason": "Give@20200910",
+            "notes": "MaximumDeltaServer"
+        },	
+        "b5fe71bf-73ed-4c49-82bf-b5688f9e574b": {
+            "reason": "Give@20200910",
+            "notes": "Ledena"
+        },
+       "360554d8-ed4b-4dca-bf86-6f928fc429d8": {
+            "reason": "Give@20200910",
+            "notes": "だてんし るしふぁ"
+        },
+		"0dfc5abc-68d1-40b4-a2b1-4791999aa464": {
+            "reason": "Give@20200910",
+            "notes": "Mahir Abdurahiman"
+        },
+        "7c8b2a0f-d4a7-4eff-bfao-d892921e5d48": {
+            "reason": "Give@20200910",
+            "notes": "DevaRei"
+        },
+        "f63d05cd-3322-4ed7-b8f5-795e2feb3dbb": {
+            "reason": "Give@20200910",
+            "notes": "PotatoGirl"
+        },		
+        "d7385406-7a71-498d-a613-3faf090f6ccb": {
+            "reason": "Give@20200910",
+            "notes": "VictoriaAiroFbGoogleaccount"
+        },		
+        "40252edd-4ec4-41a0-865f-aa9d967a3706": {
+            "reason": "Give@20200910",
+            "notes": "VictoriaAiroFbFacebookaccount"
+        },	 
+        "cfff71aa-efa6-45c2-bcaa-4e68ce796a58": {
+            "reason": "Give@20200910",
+            "notes": "MuhAdits"
+        },	
+        "3361c3f4-3d94-49d2-b19c-c1b8241d4574": {
+            "reason": "Give@20200910",
+            "notes": "GioBartolo"
+        },		
+        "3caec878-4009-4349-9bb8-9ce16cebbe98": {
+            "reason": "Give@20200910",
+            "notes": "ZainMohammed"
+        },	
+        "ba288e41-27b2-4da6-b97c-56409e073894": {
+            "reason": "Give@20200910",
+            "notes": "RavagerCanos"
+        },		
+        "693edb8d-427c-4453-a92d-2d8bd6254750": {
+            "reason": "Give@20200910",
+			"notes": "Chosenbyddevilowo-イイヅカマユミ"	
+        },	
+        "b4dc260f-f14d-479b-a373-63beb131a122": {
+            "reason": "Give@20200910",
+			"notes": "Chosenbyddevilowo-LamPhạm"	
+        },   
+        "5dc487ab-f8bb-4834-a311-fff3fc924948": {
+            "reason": "Give@20200910",
+			"notes": "Chosenbyddevilowo-抹茶"	
+        },   
+        "a9d42b13-df83-4a4d-b175-ec9dedaaaf33": {
+            "reason": "Give@20200910",
+			"notes": "Chosenbyddevilowo-wenlong"	
+        },  
+        "7f350142-5c90-473f-8eff-9352680ba8e6": {
+            "reason": "Give@20200910",
+			"notes": "Chosenbyddevilowo-kaizen"	
+        },   
+        "8ba00967-ef20-4314-b451-d802e4817eb7": {
+            "reason": "Give@20200910",
+			 "notes": "Chosenbyddevilowo-綾辻芙月渚"	
+        },     
+          "8edf92ef-1fed-4022-948c-a1308cfa3adf": {
+            "reason": "Give@20200926",
+            "notes": "ChosenbyJimboy-Judah"	
+        },       
+        "f27c8c1a-77d4-4641-9160-cf0d6f7281da": {
+            "reason": "Give@20200910",
+            "notes": "Chosenbyddevilowo-ALI-cicineLC"			
+        },   
+        "84fda50e-63c4-41d7-9ba3-e6750cb0d94e": {
+            "reason": "Give@20200910",
+            "notes": "Chosenbyddevilowo-tawis"
+        }, 
+        "3a13f17c-0c9b-4c1c-92a5-a66854621986": {
+            "reason": "Give@20200910",
+            "notes": "Chosenbyddevilowo-mako420"
+        },   
+        "a21f4130-3474-4331-add1-bd02155e607c": {
+            "reason": "Give@20200910",
+            "notes": "Chosenbyddevilowo-sondick"
+        },   
+        "8edf92ef-1fed-4022-948c-a1308cfa3adf": {
+            "reason": "Give@20200910",
+            "notes": "Chosenbyddevilowo-haine"
+        },		
+        "e0c362ab-c4f8-4ba3-8deb-fb7651fdb818": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-IamAlb"
+        }, 
+        "7ce1ef8e-54de-43b0-888f-c17a946ff8f": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-LcAuto"
+        },  
+        "8b40dce8-a7a1-420b-a30d-bb03c5a4d61a": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-AbdUllah"
+        }, 
+        "a20599a0-eb0c-428c-8fc1-17feed9b0ce6": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-AbdUllah2"
+        }, 		
+        "ff5cd59d-ab25-456c-987f-fb4ae3a0de2b": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-AlexisGalarza"
+        }, 		
+        "2ab3c1b6-3e83-41aa-aea8-7f2fc29536e0": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-HaseebSultan"
+        },
+        "a71d2c6d-6e9b-471b-abd1-e22daceabc3a": {
+            "reason": "Junky",
+            "notes": "emmanuil"
+        }, 	    
+        "02279fd0-ce99-444e-9f94-788ca4674509": {
+            "reason": "WinnerPrizeQuestsCompleted",
+            "notes": "WinnerPrize-ChristhopherBrandhon"
+        }, 	
+        "8a6280fb-1cbb-4ee0-9bc4-8f4218e422b2": {
+            "reason": "WinnerPrizeAccountAge",
+            "notes": "WinnerPrize-ChristhopherBrandhon"
+        },	
+        "ca82d506-8cdc-4794-805a-df282f9bb98d": {
+            "reason": "WinnerPrizeLongestTimeAlive",
+            "notes": "WinnerPrize-DiegoSouza"
+        },
+        "d8996a83-40c6-4aa5-b2e5-8658f7e3ad0c": {
+            "reason": "WinnerPrizeMassConsumedMassCellsEaten",
+            "notes": "WinnerPrize-AlexAlex"
+        }, 		
+        "d0816c5a-bad6-492f-8ad5-ab5a95e87426": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-Aditya_1"
+        }, 	
+        "4fefe166-87e5-4981-b5ea-8d0032f88b7d": {
+            "reason": "WinnerPrizeMassDisputeOverPrizes",
+            "notes": "WinnerPrize-Arejaytee"
+        }, 		
+        "7c8b2a0f-d4a7-4eff-bfao-d892921e5d48": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-Reis"
+        }, 	
+        "6436daae-7834-4e01-9022-d422e8ea4efe": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-Dorian"
+        }, 	
+        "1f8d6b11-3247-47b7-8e6f-03b43a0b4464": {
+            "reason": "Give@20200910",
+            "notes": "RewardDay-ChristianDolot"
+        }, 				 			
+        "4e50ef3d-626e-4bf6-a376-2a0fc0629864": {
+            "reason": "Donate",
+            "notes": "Lizzie"	
+		},
+		"7c8bc4e2-3b20-4f92-b929-d80033150dbb": {
+	    "reason": "drlazzam",
+	    "notes": "jimboy3100"		
+		},		
+		"c622e806-974f-42d5-b2d4-dc243ceebdc5": {
+	    "reason": "streamerlme",
+	    "notes": "junkyxl"		
+		},
+        "4e50ef3d-626e-4bf6-a376-2a0fc0629864": {
+            "reason": "lcplayer",
+            "notes": "junkyxl"		
+		},
+	"1ce18927-67cd-4eda-ab4e-d7d32cbf0b0d": {
+            "reason": "ahmetkalendar",
+            "notes": "junkyxl"		
+		},
+    "versionsInfo": ["v-0.3", "LM-v2.5"]
+}};	
+	
+	
